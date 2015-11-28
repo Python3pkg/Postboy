3c0a5f0202e2446edfc29581b8cdcf979edf3c6a
meng <pingf0@gmail.com>
2015-11-28 16:55:05 +0800

    update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@
 from postboy2 import PostBoy
 
 if __name__ == '__main__':
-    data = PostBoy('www.baidu.com').get()
     result = PostBoy('www.douban.com','utf-8').get()
     print(result['data']['title'])
